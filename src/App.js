506126de759cf9c9518e47561af481c8c7de6a2a
Andrii Kalashnyk <andreykalash89@gmail.com>
2023-09-14 13:19:46 +0200

    start using git flow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map((item) => (
+          <div key={item.name}>{item.name}</div>
+        ))}
+      </main>
     </div>
   );
 }
